6bc0a966b1a9d680c6b499c0c7f38964c54a4386
reyrizki <reynaldirizki97.rr@gmail.com>
2021-10-07 22:42:31 +0700

    feature(Intensity): Color Intensity Feature
    - Add preview
    - Add sliders
    - Connect to OpenCV
    - Display result

--- src/components/rgb-modal/index.jsx
+++ src/components/rgb-modal/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Modal, Button, Container } from 'react-bootstrap';
+import { Modal, Button, Container, Row, Col, Form } from 'react-bootstrap';
+
+import './styles.scss';
 
 export default class RgbModal extends React.Component {
   constructor(props) {
@@ -7,9 +9,11 @@ export default class RgbModal extends React.Component {
 
     this.state = {
       preview: null,
+      rgbValues: [0, 0, 0],
     }
 
     this.loadPreview = this.loadPreview.bind(this);
+    this.changeIntensity = this.changeIntensity.bind(this);
   }
 
   loadPreview() {
@@ -18,11 +22,53 @@ export default class RgbModal extends React.Component {
 
     this.props.cv.imshow('canvas-preview', image);
     this.setState({
-      preview: image
+      preview: image,
+      rgbValues: [0, 0, 0],
     });
   }
 
+  changeIntensity() {
+    const adjustValue = (x, n) => {
+      x += n;
+
+      if (x > 255) {
+        x = 255;
+      }
+
+      if (x < 0) {
+        x = 0;
+      }
+
+      return x;
+    }
+
+    let image = this.state.preview;
+    for (let row = 0; row < image.rows; row++) {
+      for (let col = 0; col < image.cols; col++) {
+        if (image.isContinuous()) {
+          const pos = row * image.cols * image.channels() + col * image.channels();
+
+          for (let i = 0; i < 3; i++) {
+            image.data[pos + i] = adjustValue(image.data[pos + i], this.state.rgbValues[i]);
+          }
+        }
+      }
+    }
+
+    this.props.cv.imshow("canvas-preview", image);
+  }
+
+  copyImage() {
+    const destination = document.getElementById('canvas-output');
+    const destinationContext = destination.getContext('2d');
+
+    const source = document.getElementById('canvas-preview');
+    destinationContext.drawImage(source, 0, 0, source.width, source.height);
+  }
+
   render() {
+    const colors = ["Red", "Green", "Blue"];
+
     return (
       <Modal
         show={this.props.isModalShowed}
@@ -39,6 +85,49 @@ export default class RgbModal extends React.Component {
           <Container className="d-flex">
             <canvas id="canvas-preview" className="mx-auto"></canvas>
           </Container>
+          <Container id="range-container">
+            {this.state.rgbValues.map((value, index) => {
+              return (
+                <Row key={index}>
+                  <Col lg={1}>
+                    {colors[index]}
+                  </Col>
+                  <Col lg={10}>
+                    <Form.Range
+                      min={-255}
+                      max={255}
+                      value={value}
+                      onChange={(e) => {
+                        let rgbValues = this.state.rgbValues;
+                        rgbValues[index] = e.target.value
+
+                        this.setState({
+                          rgbValues: rgbValues
+                        })
+                      }}
+                    />
+                  </Col>
+                  <Col lg={1} className="d-flex justify-content-end">
+                    {value}
+                  </Col>
+                </Row>
+              )
+            })}
+          </Container>
+          <Container className="d-flex justify-content-end gap-2">
+            <Button
+              variant="outline-dark"
+              onClick={() => this.loadPreview()}
+            >
+              Reset
+            </Button>
+            <Button
+              variant="dark"
+              onClick={() => this.changeIntensity()}
+            >
+              Apply
+            </Button>
+          </Container>
         </Modal.Body>
         <Modal.Footer>
           <Button
@@ -47,7 +136,15 @@ export default class RgbModal extends React.Component {
           >
             Cancel
           </Button>
-          <Button variant="dark">OK</Button>
+          <Button
+            variant="dark"
+            onClick={() => {
+              this.copyImage();
+              this.props.hideModal();
+            }}
+          >
+            OK
+          </Button>
         </Modal.Footer>
       </Modal>
     );
